54ba49cf5a553411d01309911d34aa2defcd869e
Josie Thompson <dot.slash.josie@gmail.com>
2023-07-04 15:48:31 -0700

    added dependency bfs

--- sketch.cpp
+++ sketch.cpp
@@ -6,9 +6,11 @@
 #include <algorithm>
 #include <ranges>
 
+#include <array>
 #include <utility>
 #include <unordered_set>
 #include <unordered_map>
+#include <deque>
 
 #include <entt/entt.hpp>
 
@@ -47,7 +49,18 @@ public:
 };
 
 using first = order_system<order::first>;
-using second = order_system<order::second>;
+
+class second : public order_system<order::second> {
+public:
+
+    template<std::output_iterator<entt::id_type> TypeOutput>
+    static TypeOutput dependencies(TypeOutput into_types)
+    {
+        namespace ranges = std::ranges;
+        return ranges::copy(std::array{ entt::type_hash<first>::value() },
+                            into_types).out;
+    }
+};
 
 template<typename T, std::output_iterator<entt::id_type> TypeOutput>
 constexpr bool has_dependencies =
@@ -78,26 +91,102 @@ public:
     void add()
     {
         auto& component = registry.emplace<Subsystem>(system_id);
-        subsystems.push_back(&component);
+        subsystems.emplace(entt::type_hash<Subsystem>::value(), &component);
         declare_dependencies<Subsystem>();
     }
     void load()
     {
         namespace ranges = std::ranges;
-        ranges::for_each(subsystems, &subsystem::load);
+        for_each(&subsystem::load);
     }
     void destroy()
     {
         namespace views = std::views; namespace ranges = std::ranges;
-        ranges::for_each(subsystems | views::reverse, &subsystem::destroy);
+        rfor_each(&subsystem::destroy);
     }
 private:
+    using subsystem_map = std::unordered_map<entt::id_type, subsystem*>;
+
     using dependency_set = std::unordered_set<entt::id_type>;
     using dependency_inserter_t = std::insert_iterator<dependency_set>;
 
     using dependency_node = std::pair<dependency_set, dependency_set>;
     using dependency_graph =
         std::unordered_map<entt::id_type, dependency_node>;
+
+    enum class direction { forward, reverse };
+
+    template<direction Direction, std::invocable<subsystem*> Visitor>
+    void bfs(Visitor visit)
+    {
+        namespace ranges = std::ranges; namespace views = std::views;
+
+        std::deque<dependency_graph::value_type> next;
+        if constexpr (Direction == direction::forward) {
+            ranges::copy_if(dependencies, std::back_inserter(next),
+                            &system_manager::is_root);
+        }
+        else {
+            ranges::copy_if(dependencies, std::back_inserter(next),
+                            &system_manager::is_leaf);
+        }
+        std::unordered_set<entt::id_type> seen;
+        ranges::transform(next, std::inserter(seen, seen.begin()),
+                          &dependency_graph::value_type::first);
+
+        auto not_seen = [this, &seen](entt::id_type id) {
+            return dependencies.contains(id) and not seen.contains(id);
+        };
+
+        std::unordered_set<entt::id_type> visited;
+        while (not next.empty()) {
+
+            auto from = next.front();
+            next.pop_front();
+
+            dependency_set* parents;
+            dependency_set* children;
+            if constexpr (Direction == direction::forward) {
+                parents = &from.second.first;
+                children = &from.second.second;
+            }
+            else {
+                parents = &from.second.second;
+                children = &from.second.first;
+            }
+
+            auto find_elem = [this](const auto& id) {
+                return *dependencies.find(id);
+            };
+            if (ranges::any_of(*parents, not_seen)) {
+                ranges::transform(*parents | views::filter(not_seen),
+                                  std::front_inserter(next), find_elem);
+                continue;
+            }
+            // only visit after all parents have been visited,
+            // and only visit once
+            if (not visited.contains(from.first)) {
+                std::invoke(visit, subsystems.at(from.first));
+                visited.insert(from.first);
+            }
+            ranges::transform(*children | views::filter(not_seen),
+                              std::back_inserter(next), find_elem);
+
+            ranges::copy(*children, std::inserter(seen, seen.begin()));
+        }
+    }
+
+    template<std::invocable<subsystem*> Visitor>
+    void for_each(Visitor visit)
+    {
+        bfs<direction::forward>(visit);
+    }
+
+    template<std::invocable<subsystem*> Visitor>
+    void rfor_each(Visitor visit)
+    {
+        bfs<direction::reverse>(visit);
+    }
 
     template<typename T>
     void declare_dependencies()
@@ -118,8 +207,8 @@ private:
             if (auto search = dependencies.find(dependent_id);
                      search != dependencies.end()) {
 
-                dependency_node& node = search->second;
-                node.second.insert(subsystem_id);
+                dependency_node& parent = search->second;
+                parent.second.insert(subsystem_id);
             }
         }
         read_dependents<T>(std::inserter(node.second, node.second.begin()));
@@ -141,9 +230,18 @@ private:
                                  &dependency_graph::value_type::first).out;
     }
 
+    static bool is_root(const dependency_graph::value_type& elem)
+    {
+        return elem.second.first.empty();
+    }
+    static bool is_leaf(const dependency_graph::value_type& elem)
+    {
+        return elem.second.second.empty();
+    }
+
     entt::registry registry;
     entt::entity system_id;
-    std::vector<subsystem*> subsystems;
+    std::unordered_map<entt::id_type, subsystem*> subsystems;
     dependency_graph dependencies;
 };
 
