8075bcae4e72300d62130c32b8e84be6845400a1
Josie Thompson <dot.slash.josie@gmail.com>
2023-07-04 11:14:47 -0700

    added subsystem interface and trivial examples

--- sketch.cpp
+++ sketch.cpp
@@ -1,6 +1,43 @@
 #include <cstdio>
 
+class subsystem {
+public:
+    virtual void load() = 0;
+    virtual void destroy() = 0;
+};
+
+class first : public subsystem {
+public:
+    virtual void load() override
+    {
+        std::printf("Load first\n");
+    }
+    virtual void destroy() override
+    {
+        std::printf("Destroy first\n");
+    }
+};
+
+class second : public subsystem {
+public:
+    virtual void load() override
+    {
+        std::printf("Load second\n");
+    }
+    virtual void destroy() override
+    {
+        std::printf("Destroy second\n");
+    }
+};
+
 int main()
 {
-    std::printf("Hello, world!\n");
+    first f;
+    second s;
+
+    f.load();
+    s.load();
+
+    s.destroy();
+    f.destroy();
 }
